1d095ddbd9e70869e21f67a082ef4065d7bfac99
far1ca <77165212+far1ca@users.noreply.github.com>
2025-07-28 14:47:29 +0200

    Adjust icon, title, and add some logic for form & bookshelf

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 
 Angular + Express website for displaying your very own bookshelf
 
-Want to display all the books you've read on a bookshelf? Look no further! Using this website you can add books to your very own bookshelf. Customize the size and color, add comments/reviews for each book, and enjoy the cozy result!
+Want to display all the books you've read on a bookshelf? Look no further! Using this website you can add books to your very own bookshelf. Customize the color, add comments/reviews for each book, and enjoy the cozy result!
 
 Credits to https://codepen.io/ashleynolan/pen/mdxBpZ for the cute cat anim!
--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,56 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  inject,
+  signal,
+  TemplateRef,
+  WritableSignal,
+} from '@angular/core';
+
+import { ModalDismissReasons, NgbModal } from '@ng-bootstrap/ng-bootstrap';
+
+interface Book {
+  name: string;
+  color: string;
+  desc: string;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'client';
+  title = 'virtual bookshelf';
+  private modalService = inject(NgbModal);
+  closeResult: WritableSignal<string> = signal('');
+
+  submitForm(form: any): void {
+    console.log(form.form.value);
+    form.reset();
+    this.modalService.dismissAll();
+  }
+
+  open(content: TemplateRef<any>) {
+    this.modalService
+      .open(content, { ariaLabelledBy: 'modal-basic-title' })
+      .result.then(
+        (result) => {
+          this.closeResult.set(`Closed with: ${result}`);
+        },
+        (reason) => {
+          this.closeResult.set(`Dismissed ${this.getDismissReason(reason)}`);
+        }
+      );
+  }
+
+  private getDismissReason(reason: any): string {
+    switch (reason) {
+      case ModalDismissReasons.ESC:
+        return 'by pressing ESC';
+      case ModalDismissReasons.BACKDROP_CLICK:
+        return 'by clicking on a backdrop';
+      default:
+        return `with: ${reason}`;
+    }
+  }
 }
