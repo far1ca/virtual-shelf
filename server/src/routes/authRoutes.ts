b040a2667f1cffa7574c055180e0b95caae4fbc0
far1ca <77165212+far1ca@users.noreply.github.com>
2025-07-30 08:58:32 +0200

    Add basic server logic

---
+++ server/src/routes/authRoutes.ts
@@ -0,0 +1,23 @@
+import express from "express";
+import passport from "passport";
+const router = express.Router();
+
+router.get("/login", (req, res) => {
+  if (req.user) {
+    res.redirect("/profile");
+  }
+  res.render("login");
+});
+
+router.get(
+  "/google",
+  passport.authenticate("google", {
+    scope: ["email", "profile"],
+  })
+);
+
+router.get("/google/redirect", passport.authenticate("google"), (req, res) => {
+  res.redirect("/profile");
+});
+
+export default router;
